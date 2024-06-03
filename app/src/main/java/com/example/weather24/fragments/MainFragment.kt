4747f86997e5f92b23f1ea30811fc17ef62b5c47
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-03 18:18:40 +0600

    Add location permission.

--- app/src/main/java/com/example/weather24/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weather24/fragments/MainFragment.kt
@@ -1,16 +1,21 @@
 package com.example.weather24.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.weather24.R
 import com.example.weather24.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var binding : FragmentMainBinding
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,30 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private  fun checkPermission() {
+        //check if user granted or denied permission
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            //init permissionLauncher
+            permissionListener()
+
+            //permission request dialog
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
